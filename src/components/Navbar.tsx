4ef395c893db863ca4f226bcdf444f8e41d10789
Abdelrhman Adel <abdelrhmanadel2462005@gmail.com>
2024-09-26 16:35:45 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
-"use client";
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
+"use client"; // Add this line at the top
+import Image from 'next/image'; 
+import Link from 'next/link'; 
+import { useState, useEffect } from 'react'; 
 import { FiHome, FiInfo, FiDollarSign, FiUser, FiMenu, FiX, FiChevronDown } from 'react-icons/fi';
 
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
 
-  let timeoutId: string | number | NodeJS.Timeout | undefined;
+  let timeoutId: string | number | NodeJS.Timeout | undefined; 
 
   const handleLinkClick = () => {
     setMobileMenuOpen(false); 
@@ -33,37 +33,33 @@ const Navbar = () => {
   };
 
   return (
-    <header className={`flex items-center justify-between shadow-md h-16 px-6 ${darkMode ? 'bg-gradient-to-r from-[#00454A] to-[#007C7E]' : 'bg-gradient-to-r from-teal-500 to-teal-700'}`}>
+    <header className={`flex items-center justify-between shadow-md h-16 ${darkMode ? 'bg-gradient-to-r from-[#00454A] to-[#007C7E]' : 'bg-gradient-to-r from-teal-500 to-teal-700'}`}>
       <div className="flex-shrink-0">
         <Link href="/">
-          <Image src="/images/c186678f7cd589c185fff8baa189e685.png" alt="Logo" className="h-10" width={80} height={80} />
+        < Image src="/images/c186678f7cd589c185fff8baa189e685.png" alt="Logo" className="ml-10 h-10" width={100} height={100} />
         </Link>
       </div>
 
-      {/* Hamburger Button */}
       <button 
-        className={`sm:hidden focus:outline-none ${darkMode ? 'text-gray-300' : 'text-white'}`} 
+        className={`sm:hidden  ${darkMode ? 'text-white' : 'text-white'}  mr-5`} 
         onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
         aria-expanded={mobileMenuOpen}
         aria-label="Toggle navigation"
       >
-        {mobileMenuOpen ? <FiX size={26} /> : <FiMenu size={26} />}
+        {mobileMenuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
       </button>
 
-      {/* Mobile menu backdrop */}
       {mobileMenuOpen && (
-        <div className="fixed inset-0 bg-black opacity-50 z-10" onClick={handleLinkClick}></div>
+        <div className="fixed inset-0 bg-black opacity-30 z-10" onClick={handleLinkClick} />
       )}
 
-      {/* Main Navigation */}
       <nav 
-        className={`${
-          mobileMenuOpen ? 'flex' : 'hidden'
-        } sm:flex flex-col sm:flex-row items-center absolute sm:static top-16 right-0 w-full sm:w-auto bg-white sm:bg-transparent z-20 transition-all duration-500 ease-in-out sm:ml-10`}
+        className={`relative flex items-center ml-10 transition-all duration-200 ${mobileMenuOpen ? ' flex-col absolute top-16 left-0 w-full bg-teal-500 z-20 sm:static sm:flex-row' : 'hidden sm:flex'}`}
         role="navigation"
+        
       >
         {/* Dark Mode Toggle */}
-        <div className="flex items-center mb-4 sm:mb-0">
+        <div className="flex items-center ml-4">
           <label className="relative inline-flex items-center cursor-pointer">
             <input type="checkbox" className="sr-only" checked={darkMode} onChange={toggleDarkMode} />
             <div className={`w-11 h-6 rounded-full shadow-inner ${darkMode ? 'bg-gray-800' : 'bg-gray-200'}`}></div>
@@ -71,33 +67,32 @@ const Navbar = () => {
           </label>
         </div>
 
-        {/* Links */}
-        <Link href="/" className={`flex items-center px-4 py-2 ${darkMode ? 'text-gray-300 hover:bg-gray-700' : 'text-gray-700 sm:text-white hover:bg-gray-200'} transition duration-300 mx-2 rounded-lg`} onClick={handleLinkClick}>
-          <FiHome className="mr-2" /> Home
+        <Link href="/" className={`flex items-center ${darkMode ? 'text-gray-300 hover:text-white' : 'text-white hover:text-gray-200'} transition duration-200 mx-2 p-2 rounded-md`} onClick={handleLinkClick}>
+          <FiHome className="mr-1" /> Home
         </Link>
 
-        <Link href="https://gammal.tech" className={`flex items-center px-4 py-2 ${darkMode ? 'text-gray-300 hover:bg-gray-700' : 'text-gray-700 sm:text-white hover:bg-gray-200'} transition duration-300 mx-2 rounded-lg`} onClick={handleLinkClick}>
-          <FiInfo className="mr-2" /> About
+        <Link href="https://www.gammal.tech/" className={`flex items-center ${darkMode ? 'text-gray-300 hover:text-white' : 'text-white hover:text-gray-200'} transition duration-200 mx-2 p-2 rounded-md`} onClick={handleLinkClick}>
+          <FiInfo className="mr-1" /> About
         </Link>
 
-        <Link href="/pricing" className={`flex items-center px-4 py-2 ${darkMode ? 'text-gray-300 hover:bg-gray-700' : 'text-gray-700 sm:text-white hover:bg-gray-200'} transition duration-300 mx-2 rounded-lg`} onClick={handleLinkClick}>
-          <FiDollarSign className="mr-2" /> Pricing
+        <Link href="/pricing" className={`flex items-center ${darkMode ? 'text-gray-300 hover:text-white' : 'text-white hover:text-gray-200'} transition duration-200 mx-2 p-2 rounded-md`} onClick={handleLinkClick}>
+          <FiDollarSign className="mr-1" /> Pricing
         </Link>
 
-        {/* Dropdown */}
         <div 
-          className="relative mb-4 sm:mb-0" 
+          className="relative" 
           onMouseEnter={handleMouseEnterDropdown} 
           onMouseLeave={handleMouseLeaveDropdown}
         >
-          <button className={`flex items-center px-4 py-2 ${darkMode ? 'text-gray-300 hover:bg-gray-700' : 'text-gray-700 sm:text-white hover:bg-gray-200'} mx-2 rounded-lg`}>
-            <FiUser className="mr-2" /> User <FiChevronDown className="ml-1" />
+          <button className={`flex items-center ${darkMode ? 'text-gray-300 hover:text-white' : 'text-white hover:text-gray-200'} mx-2 p-2 rounded-md`}>
+            <FiUser className="mr-1" /> User <FiChevronDown className="ml-1" />
           </button>
           {dropdownOpen && (
-            <div className={`absolute right-0 mt-2 rounded-md shadow-lg ${darkMode ? 'bg-gray-800 text-gray-300' : 'bg-white text-black'} z-30`}>
+            <div className={`absolute right-0 ${darkMode ? 'bg-gray-800 text-gray-300' : 'bg-white text-black'} shadow-lg rounded-md mt-2`}>
               <Link href="/profile" className={`block px-4 py-2 ${darkMode ? 'hover:bg-gray-700' : 'hover:bg-gray-200'}`} onClick={handleLinkClick}>Profile</Link>
               <Link href="/certifcate" className={`block px-4 py-2 ${darkMode ? 'hover:bg-gray-700' : 'hover:bg-gray-200'}`} onClick={handleLinkClick}>Certificates</Link>
               <Link href="/logout" className={`block px-4 py-2 ${darkMode ? 'hover:bg-gray-700' : 'hover:bg-gray-200'}`} onClick={handleLinkClick}>Logout</Link>
+            
             </div>
           )}
         </div>
@@ -107,12 +102,13 @@ const Navbar = () => {
           <span 
             className={`${
               darkMode ? 'bg-[#003545] text-white hover:bg-[#071E3D]' : 'bg-[#FCE09B] text-black hover:bg-[#FFEEA9]'
-            } px-6 py-2 flex items-center justify-center hover:shadow-lg transition duration-300 rounded-full`} 
-            style={{ minWidth: '100px', minHeight: '45px', fontWeight: '600', fontSize: '16px' }}
+            } px-4 py-2 flex items-center justify-center hover:shadow-lg transition duration-200 my-2 sm:my-0 sm:ml-6 mr-10 rounded-full`} 
+            style={{ minWidth: '94px', minHeight: '43px', fontWeight: '600', fontSize: '16px' }}
           >
-            <FiUser className="mr-2" /> Sign In
+            <FiUser className="mr-1" /> Sign In
           </span>
         </Link>
+        
       </nav>
     </header>
   );
